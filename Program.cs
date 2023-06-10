95bafebbe2ecd5d5ab27209c561ea27184a82012
Abhinav Kumar <kabhinav713@gmail.com>
2023-06-10 09:04:06 +0530

    [Abh]-adding uc1 part

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Enter name");
+string name = Console.ReadLine();
+Console.WriteLine(name);
+
+//uc1
+
+string namepattern = "^[A-Z]{3,}$";
+
+
+Regex regex = new Regex(namepattern);
+
+if (regex.IsMatch(name))
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine("Valid");
+    Console.ResetColor();
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.WriteLine("InValid");
+    Console.ResetColor();
+}
